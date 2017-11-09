f0147764f17ea8c5df78335660e9ed588a8191c2
jnshuti93 <jnshuti93@yahoo.com>
2017-11-09 21:29:00 +0100

    First commit: adding gradle java project

---
+++ src/main/java/main/Calculator.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * @author Jean-Rene Nshuti
+ * this program was taken from https://openclassrooms.com/courses/les-tests-unitaires-en-java
+ * multiply and subtract classes as well as tests
+ * will be implemented by me.
+ * The rest is from openclassrooms
+ *
+ *
+ */
+public interface Calculator {
+
+    int multiply(int a, int b);
+    int divide(int a, int b);
+    int add(int a, int b);
+    int substract(int a, int b);
+
+}
